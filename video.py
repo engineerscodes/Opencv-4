29ec5cdf75ea8855eee1296a882adfa7956bc6f0
engineerscodes <practicalengineers05@gmail.com>
2020-09-14 14:35:07 +0530

    initial commit

--- video.py
+++ video.py
@@ -2,13 +2,14 @@ import argparse
 from cv2 import *
 
 arg=argparse.ArgumentParser()
-arg.add_argument("--input",help="enter video path")
+arg.add_argument("--input",help="enter video path",default=0)
 args=vars(arg.parse_args())
 cap=VideoCapture(0)
 
 while True:
     reg,frame=cap.read()
-
+    if reg is None:
+        break
     cv2.imshow("OUPUT IMAGE",frame)
     if cv2.waitKey(100) & 0xFF==ord('q'):
         break
