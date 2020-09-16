ddb85e6497ef76632c2d6c145c1e74b7f1925596
engineerscodes <practicalengineers05@gmail.com>
2020-09-16 10:25:03 +0530

    initial commit

--- acessing_cap_pro.py
+++ acessing_cap_pro.py
@@ -5,7 +5,10 @@ arg=argparse.ArgumentParser()
 arg.add_argument("--input",help='Video paths ',default=0)
 args=vars(arg.parse_args())
 cap=VideoCapture(args['input'])
-
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')) # depends on fourcc available camera
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(CAP_PROP_FPS,15)#still fps doesnot https://stackoverflow.com/questions/37442069/cap-prop-fps-doesnt-change-in-opencv-3
 width=cap.get(CAP_PROP_FRAME_WIDTH)
 heigth=cap.get(CAP_PROP_FRAME_HEIGHT)
 FPS=cap.get(CAP_PROP_FPS)
@@ -14,7 +17,7 @@ print('frame heigth of video {}  \nFPS OF VIDEO :{}'.format(heigth,FPS))
 index=0
 while True:
     check,frame=cap.read()
-    imshow("display",frame)
+    imshow("display",cvtColor(frame,COLOR_BGR2LAB))
     if waitKey(100) & 0xFF==ord('q'):
         break
     if waitKey(20) & 0xFF==ord('c'): #writing frame to local drive if 'c'is pressed,plz change the path to your drive
