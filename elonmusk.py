0e813279936028eaa8adbdc280aa6871313ee7d1
engineerscodes <practicalengineers05@gmail.com>
2020-09-16 14:55:16 +0530

    initial commit

--- elonmusk.py
+++ elonmusk.py
@@ -1,7 +1,7 @@
 import cv2
 
 img=cv2.imread('venv\image\dpelonmask.jpeg')
-img=cv2.resize(img,(400,400))
+img=cv2.resize(img,(500,400))
 cv2.imshow("git image",img)
 cv2.imwrite('venv\image\git.jpeg',img)
 cv2.waitKey(0)
--- write_video.py
+++ write_video.py
@@ -1,14 +1,15 @@
 from cv2 import *
 
 cap=VideoCapture(0)
-cap.set(CAP_PROP_FRAME_WIDTH,500)
-cap.set(CAP_PROP_FRAME_HEIGHT,500)
-fourcc=VideoWriter_fourcc('M','P','4','V') #fourcc of mp4 is  0x7634706d
-gray=VideoWriter('naveen.mp4',0x7634706d,int(30),(500,500),False)
+'''cap.set(CAP_PROP_FRAME_WIDTH,500)
+cap.set(CAP_PROP_FRAME_HEIGHT,500)'''
+fourcc=VideoWriter_fourcc('X','V','I','D') #fourcc of mp4 is  0x7634706d
+gray=VideoWriter('naveen.avi',fourcc,int(30),(1020,1020),False)
 while True:
     check,frame=cap.read()
     if check is True:
         back=cvtColor(frame,COLOR_BGR2GRAY)
+        back=cv2.resize(back, (1020,1020))#VideoWriter('naveen.avi',fourcc,int(30),(1020,1020),False) both frame size and video cap size must be same
         imshow("video",back)
         gray.write(back)
         if waitKey(100) & 0xFF==ord('q'):
